95ef54855c113b9dca5832c3635160fc68ff779c
Caleb Maclennan <caleb@alerque.com>
2025-11-28 16:01:21 +0300

    Enable syntax highlighting in CodeMirror

--- src/codemirror-bundle.js
+++ src/codemirror-bundle.js
@@ -1,41 +1,60 @@
 import { EditorView, lineNumbers } from "@codemirror/view";
 import { EditorState } from "@codemirror/state";
-import { basicSetup } from "@codemirror/basic-setup";
-import { xml } from "@codemirror/lang-xml";
-import { html } from "@codemirror/lang-html";
+import { syntaxHighlighting, defaultHighlightStyle } from "@codemirror/language";
 import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
 import { javascript } from "@codemirror/lang-javascript";
+import { markdown } from "@codemirror/lang-markdown";
+import { xml } from "@codemirror/lang-xml";
+import { yaml } from "@codemirror/lang-yaml";
+import { latex } from "codemirror-lang-latex";
+import { typst } from "codemirror-lang-typst";
 
-window.initCodeMirror = function (element, content, language = "text") {
-	const languageMap = {
-		xml: xml(),
-		html: html(),
-		css: css(),
-		javascript: javascript(),
-	};
+const languageMap = {
+	// Official CodeMirror languages
+	css: css(),
+	html: html(),
+	javascript: javascript(),
+	markdown: markdown(),
+	xml: xml(),
+	yaml: yaml(),
+	// Unofficial 3rd party languages
+	latex: latex(),
+	typst: typst(),
+	// Aliases to near-matches
+	pagedjs: html(),
+	sile: latex(),
+	weasyprint: html(),
+	xelatex: latex(),
+};
 
+window.initCodeMirror = function (element, content, language) {
 	let polyTheme = EditorView.baseTheme({
 		"&light": {
-			backgroundColor: "#fff"
+			backgroundColor: "#fff",
 		},
 		"&dark": {
-			backgroundColor: "#000"
-		}
+			backgroundColor: "#000",
+		},
 	});
 
 	const extensions = [
-		basicSetup,
-		lineNumbers(),
-		languageMap[language] || [],
 		EditorState.readOnly.of(true),
 		polyTheme,
+		lineNumbers(),
+		syntaxHighlighting(defaultHighlightStyle),
+		languageMap[language] || [],
 	];
+
+	let div = document.createElement("div");
+	div.className = element.className;
+	element.replaceWith(div);
 
 	new EditorView({
 		state: EditorState.create({
 			doc: content,
 			extensions,
 		}),
-		parent: element,
+		parent: div,
 	});
 };
