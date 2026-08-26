ab145fe898304fba0784f50927fd9740ca325efa
Caleb Maclennan <caleb@alerque.com>
2026-08-26 09:33:14 +0300

    Have nix develop drop into a Zed shell instead of bash matching the Justfile interpreter

--- flake.nix
+++ flake.nix
@@ -92,6 +92,7 @@
             make fonts
             export PUPPETEER_EXECUTABLE_PATH=${pkgs.chromium}/bin/chromium
             export PUPPETEER_ARGS="--no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage"
+            exec zsh +o nomatch -feu
           '';
         };
       }
